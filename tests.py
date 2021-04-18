9cb320b357157d70b1cb9182ff2cb9aab3c59b79
Abir Qasem <abir.qasem@gmail.com>
2021-04-18 08:31:18 -0400

    path eval for TG

--- tests.py
+++ tests.py
@@ -1,9 +1,31 @@
 import processor
 
 
+def scratch ():
+    data = {"ab":5, "cd": {"x":5, "y":"2"}}
+    path = "[\"cd\"] [\"y\"]"
+
+    print (eval("data" + path))
+
+    #print (data["cd"]["y"])
+
+
+
+
+
+
+
+
+
+
+
+
 
 def tests():
   print ("rrr")
 
+def main():
+    scripts()
+
 if __name__ == "__main__":
-    tests()
+    scratch()
